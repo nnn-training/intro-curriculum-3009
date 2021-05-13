cfbc7adda7cff5d9ef75b3ec933e59dcd0cbeffe
shuttle <yuki.rock1122@icloud.com>
2021-05-13 18:37:56 +0900

    非同期I/Oから同期I/Oへ変更

--- app.js
+++ app.js
@@ -2,19 +2,19 @@
 const fs = require('fs');
 const fileName = './test.txt';
 
-function appendFileSync(fileName, str){
-  return new Promise((resolve) => {
-    fs.appendFile(fileName, str, 'utf8', () => resolve());
-  });
-}
+// function appendFileSync(fileName, str){
+//   return new Promise((resolve) => {
+//     fs.appendFile(fileName, str, 'utf8', () => resolve());
+//   });
+// }
 
-async function greet() {
+// async function greet() {
   for (let count = 0; count < 30; count++) {
-    await appendFileSync(fileName, 'おはようございます\n', 'utf8');
-    await appendFileSync(fileName, 'こんにちは\n', 'utf8');
-    await appendFileSync(fileName, 'こんばんは\n', 'utf8');
+    fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
   }
-}
+// }
 
-greet();
+// greet();
 
