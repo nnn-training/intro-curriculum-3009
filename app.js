3ebfd389663abfdf385d3547a75600af9b0d5374
Daiki Kishi <dik.kishi.1108@gmail.com>
2021-06-16 10:52:39 +0900

    writeGreetingの実装

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 'use strict';
+
+function appendFilePromise(fs, fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  });
+}
+async function writeGreeting(fs, fileName) {
+  for (let count = 0; count < 30; count++) {
+    await appendFilePromise(fs, fileName, 'おはようございます\n');
+    await appendFilePromise(fs, fileName, 'こんにちは\n');
+    await appendFilePromise(fs, fileName, 'こんばんは\n');
+  }
+}
+
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-  fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
-}
+writeGreeting(fs, fileName);
