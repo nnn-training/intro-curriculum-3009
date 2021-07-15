5789b5f4ee9bb1dde1863094d3d516335bcdc286
AliceT2C <e-kurata@XW-000314>
2021-07-15 17:38:27 +0900

    同期 I/O　に変更

--- app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const fs = require('fs');
-const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-  fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
-}
