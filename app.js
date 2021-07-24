6d0e2524c3bae0a47bfdf5d4bb5f56eac6816d4b
Kengo Yagishita <yagishitakengo1002@gmail.com>
2021-07-24 14:39:00 +0900

    async/await関数を用いて同期I/O化

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-  fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+
+// fs.appendFileをPromiseオブジェクト化している関数appendFilePromise
+function appendFilePromise(fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  });
 }
+
+// async function でmainが非同期関数だと宣言
+// main関数内でawait演算子が使用可能
+async function main() {
+  for (let count = 0; count < 30; count++) {
+    // await演算子によって、Promiseの完了を待ってから次の行の処理へ進む
+    await appendFilePromise(fileName, 'おはようございます\n');
+    await appendFilePromise(fileName, 'こんにちは\n');
+    await appendFilePromise(fileName, 'こんばんは\n');
+  }
+}
+
+main();
