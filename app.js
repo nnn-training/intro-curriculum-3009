d4add66cc95697c00df08da341e2fd81cf93c974
吉田稲作 <yoshidanoinasaku@gmail.com>
2021-11-11 16:44:18 +0900

    非同期処理の制御

--- app.js
+++ app.js
@@ -1,8 +1,71 @@
-'use strict';
-const fs = require('fs');
-const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-  fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+/**
+ * 30回ファイルへの書き込みを行う
+ *  その際、「おはようございます」「こんにちは」「こんばんは」の順番で出力できるようにする
+ */
+ 'use strict';
+ const fs = require('fs');
+ const fileNameAppendSync = './test-append-sync.txt';
+ const fileNamePromise = './test-promise.txt';
+ const fileNameAsyncAwait  = './test-async-await.txt';
+
+ // appendFileSync
+for(let count = 0; count < 30; count++) {
+  fs.appendFileSync(fileNameAppendSync, 'おはようございます\n', 'utf8');
+  fs.appendFileSync(fileNameAppendSync, 'こんにちは\n', 'utf8');
+  fs.appendFileSync(fileNameAppendSync, 'こんばんは\n', 'utf8');
 }
+
+// promise（わからん）
+/**  自分でおこなったもの
+const appdendPromise = new Promise(resolve => {
+  fs.appendFile(fileNamePromise, 'おはようございます\n', 'utf8', () => resolve());
+})
+const appendPromise2 = new Promise(resolve => {
+  fs.appendFile(fileNamePromise, 'こんにちは\n', 'utf8', () => resolve());
+})
+const appdendPromise3 = appendPromise2.then(() => {
+  fs.appendFile(fileNamePromise, 'こんばんは\n', 'utf8', () => {});
+});
+const promiseArray = [appdendPromise, appendPromise2, appdendPromise3];
+for(let count = 0; count < 30; count++) {
+  return appendPromise3;
+}
+*/
+// 回答
+function appendFilePromise(fileName, str) {
+  return new Promise(resolve => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  })
+}
+function appendFilePromiseMain() {
+  let promiseChain = Promise.resolve();  // Promiseチェーンを記憶する変数
+  for(let count = 0; count < 30; count++) {
+    promiseChain = promiseChain
+    .then(() => {
+      return appendFilePromise(fileNamePromise, 'おはようございます\n');
+    })
+    .then(() => {
+      return appendFilePromise(fileNamePromise, 'こんにちは\n');
+    })
+    .then(() => {
+      return appendFilePromise(fileNamePromise, 'こんばんは\n');
+    })
+  }
+}
+appendFilePromiseMain();
+
+
+// async await
+function appendFileAyncAwait(fileName, str) {
+  return new Promise((resolve, reject) => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  })
+}
+async function main() {
+  for(let count = 0; count < 30; count++) {
+    await appendFileAyncAwait(fileNameAsyncAwait, 'おはようございます\n');
+    await appendFileAyncAwait(fileNameAsyncAwait, 'こんにちは\n');
+    await appendFileAyncAwait(fileNameAsyncAwait, 'こんばんは\n');
+  }
+}
+main();
