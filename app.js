57765c1cea95c290600f422e5837b7e25358c086
sinano1107 <tftf161107@icloud.com>
2022-01-31 12:10:45 +0900

    おはようございます！！！！！！！

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-  fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+
+/**
+ * ファイルに文字を書き込むプロミスを返す
+ * @param {string} fileName
+ * @param {string} str
+ * @return {Promise}
+ */
+function appendFilePromise(fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, 'utf8', resolve);
+  });
 }
+
+/**
+ * main関数
+ */
+async function main() {
+  for (let count = 0; count < 30; count++) {
+    await appendFilePromise(fileName, 'おはようございます\n');
+    await appendFilePromise(fileName, 'こんにちは\n');
+    await appendFilePromise(fileName, 'こんばんは\n');
+  }
+}
+
+main();
