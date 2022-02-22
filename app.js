451d0c5e859faab023034ea3588157ff83dac96f
kobucha <kashikomi@outlook.com>
2022-02-22 19:37:09 +0900

    同期処理にしました

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-'use strict';
-const fs = require('fs');
-const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-  fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+"use strict";
+const fs = require("fs");
+const fileName = "./test.txt";
+
+function pms(fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, "utf-8", () => resolve());
+  });
 }
+
+(async function () {
+  for (let count = 0; count < 30; count++) {
+    await pms(fileName, "おはようございます\n");
+    await pms(fileName, "こんにちは\n");
+    await pms(fileName, "こんばんは\n");
+  }
+})();
