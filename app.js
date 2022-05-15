7959e732573b6bdda23e4a4b4ae53add89fb0a4b
kazukifn <oknsdnhrkm56@gmail.com>
2022-05-15 16:54:15 +0900

    非同期I/Oから同期I/Oに変更

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 'use strict';
-const fs = require('fs');
-const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-  fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
-}
+(() => {
+  const fs = require('fs');
+  const fileName = './test.txt';
+
+  const appendFilePromise = (fileName, str) => {
+    return new Promise((resolve) => {
+      fs.appendFile(fileName, str, 'utf8', () => resolve());
+    });
+  }
+
+  const main = async () => {
+    for (let count = 0; count < 30; count++) {
+      await appendFilePromise(fileName, 'おはようございます\n', 'utf8', () => { });
+      await appendFilePromise(fileName, 'こんにちは\n', 'utf8', () => { });
+      await appendFilePromise(fileName, 'こんばんは\n', 'utf8', () => { });
+    }
+  }
+  main();
+})();
+
