67c2b1b16df1c1cf5ddbb72c3ccc3f50b721629d
Hiiro116 <107736464+Hiiro116@users.noreply.github.com>
2022-09-24 11:09:38 +0900

    async/awaitに変更

--- app.js
+++ app.js
@@ -6,19 +6,11 @@ function appendFilePromise(fileName, str) {
     fs.appendFile(fileName, str, 'utf8', () => resolve());
   });
 }
-function main() {
-  let promiseChain = Promise.resolve(); // Promise チェーンを記憶する変数
+async function main() {
   for (let count = 0; count < 500; count++) {
-    promiseChain = promiseChain
-      .then(() => {
-        return appendFilePromise(fileName, 'おはようございます\n');
-      })
-      .then(() => {
-        return appendFilePromise(fileName, 'こんにちは\n');
-      })
-      .then(() => {
-        return appendFilePromise(fileName, 'こんばんは\n');
-      });
+    await appendFilePromise(fileName, 'おはようございます\n');
+    await appendFilePromise(fileName, 'こんにちは\n');
+    await appendFilePromise(fileName, 'こんばんは\n');
   }
 }
 main();
