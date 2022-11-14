f90f7a09dcb5ce2e817715fd89f76ddc54c3c96c
takeshi <tksh0828@gmail.com>
2022-11-14 11:01:53 +0900

    async-awaitにて書き換え完了

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 "use strict";
+
+console.log("3009 started");
 const fs = require("fs");
 const fileName = "./test.txt";
 // for (let count = 0; count < 30; count++) {
@@ -7,18 +9,18 @@ const fileName = "./test.txt";
 //   fs.appendFileSync(fileName, "こんばんは\n", "utf8", () => {});
 // }
 
-function asyncFilePromise(fileName, str) {
+function appendFilePromise(fileName, str) {
   return new Promise((resolve) => {
-    fs.appendFile(fileName, str, "utf-8", resolve());
+    fs.appendFile(fileName, str, "utf8", () => resolve());
   });
 }
 
 async function main() {
   for (let i = 0; i < 50; i++) {
-    await asyncFilePromise(fileName, "おはようございます\n");
-    await asyncFilePromise(fileName, "こんにちは\n");
-    await asyncFilePromise(fileName, "こんばんは\n");
-    await asyncFilePromise(fileName, "---next day---\n");
+    await appendFilePromise(fileName, "おはようございます\n");
+    await appendFilePromise(fileName, "こんにちは\n");
+    await appendFilePromise(fileName, "こんばんは\n");
+    await appendFilePromise(fileName, "---next day---\n");
   }
 }
 main();
