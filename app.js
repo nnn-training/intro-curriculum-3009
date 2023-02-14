1ad61276efdb8c6ff7c21da9a7a666943bf6a499
akanekwon172 <akanekwon_dev@icloud.com>
2023-02-14 18:19:55 +0900

    async , await を用いて実装

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-  fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
-}
+const appendFilePromise = fileName => async str => {
+  await new Promise(resolve => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  });
+};
+
+const main = async () => {
+  const append = appendFilePromise(fileName);
+  const words = ['おはようございます\n', 'こんにちは\n', 'こんばんは\n'];
+
+  for (let count = 0; count < 500; count++) {
+    await words.reduce(
+      (promiseChain, word) => promiseChain.then(() => append(word)),
+      Promise.resolve()
+    );
+  }
+};
+
+main();
