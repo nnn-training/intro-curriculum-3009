8881f1175106a90cc707e2b278f3d2c6f282578c
Masaki Ariyoshi <arys3014@gmail.com>
2023-09-28 23:55:14 +0900

    Pseudo cat command implementation

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 'use strict';
 const fs = require('node:fs');
 
-function cat(fileName) {
-  const content = fs.readFile(fileName, 'utf8', () => {});
+/**
+ * For Promise chain
+ * function cat(fileName) {
+ *  const content = fs.promises.readFile(fileName, 'utf8').then(() => {
+ *    console.log(content);
+ *  });
+ * }
+ * cat(process.argv[2]);
+ */
+
+// For async/await
+async function cat(fileName) {
+  const content = await fs.promises.readFile(fileName, 'utf8');
   console.log(content);
 }
 
