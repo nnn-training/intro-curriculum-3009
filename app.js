cf9bbebcd0726dfd7a34c8dc996f0f6ebee74941
k-2795 <232990176+k-2795@users.noreply.github.com>
2025-10-03 13:14:33 +0900

    catコマンドを実装

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
+// fs.readFileSync
+
+// 'use strict';
+// const fs = require('node:fs');
+
+// function cat(fileName) {
+//   const content = fs.readFileSync(fileName, 'utf8');
+//   console.log(content);
+// }
+
+// promise チェーン
+
+// 'use strict';
+// const fs = require('node:fs');
+
+// function cat(fileName) {
+//   const promise = new Promise((resolve, reject) => {
+//     fs.readFile(fileName, 'utf8', (err, data) => {
+//       return resolve(data);
+//     });
+//   }).then((content) => {
+//     console.log(content);
+//   });
+// }
+
+// async/await
+
 'use strict';
 const fs = require('node:fs');
 
-function cat(fileName) {
-  const content = fs.readFile(fileName, 'utf8', () => {});
+async function cat(fileName) {
+  const content = await fs.promises.readFile(fileName, 'utf8');
   console.log(content);
 }
 
